b30a4809c6fb0b46efacd018f1b7970cf1ea347d
Ahir Anto <antuvhai@gmail.com>
2025-08-13 07:43:54 -0700

    added to sign in page captcha method

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,7 +1,27 @@
 
 import signInImg from '../assets/others/authentication2.png'
 import bgImg from '../assets/others/authentication.png'
+import { loadCaptchaEnginge, LoadCanvasTemplate, LoadCanvasTemplateNoReload, validateCaptcha } from 'react-simple-captcha';
+import { useEffect, useRef, useState } from 'react';
 const SignIn = () => {
+  const [disabled,setDisabled] = useState(true)
+  const captchaRef = useRef();
+  useEffect(()=>{
+    loadCaptchaEnginge(6);
+  },[])
+  const handleSubmit = e =>{
+    e.preventDefault()
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(email , password)
+  }
+  const handleCaptcha = () =>{
+    const value = captchaRef.current.value;
+    if(validateCaptcha(value)){
+      setDisabled(false)
+    }
+  }
     return (
         <div style={{backgroundImage:`url(${bgImg})`}} className="hero min-h-screen bg-cover bg-center inset-0">
   <div className="hero-content flex-col w-11/14 mx-auto lg:flex-row md:flex-row  shadow-2xl px-16 py-20 ">
@@ -11,13 +31,24 @@ const SignIn = () => {
     <div className="card w-full max-w-sm">
         <h1 className='text-center text-2xl font-bold'>Sign In</h1>
       <div className="card-body"> 
-        <form className="fieldset">
-          <label className="label">Email</label>
-          <input type="email" className="input" placeholder="Email" />
-          <label className="label">Password</label>
-          <input type="password" className="input" placeholder="Password" />
+        <form onSubmit={handleSubmit} className="fieldset">
+          <div>
+               <label className="label">Email</label>
+               <input type="email" name='email' className="input" placeholder="Email" />
+          </div>
+          <div>
+            <label className="label">Password</label>
+          <input type="password" name='password' className="input" placeholder="Password" />
+          </div>
+          <div>
+            <LoadCanvasTemplate />
+             <input type="text" ref={captchaRef} name='captcha' className="input" placeholder="Type the catcha above" />
+             <button onClick={handleCaptcha} className='btn hover:bg-[#d1a054] border-[#d1a054] hover:text-white btn-outline btn-xs mt-2 w-full'>Validate</button>
+          </div>
           <div><a className="link link-hover">Forgot password?</a></div>
-          <input className="btn bg-[#d1a054] text-white mt-4" type="submit" name="" value={'Sign In'} id="" />
+          <div className="form-control">
+            <input disabled={disabled} className="btn w-full bg-[#d1a054] text-white mt-4" type="submit"  value={'Sign In'} />
+          </div>
         </form>
       </div>
     </div>
