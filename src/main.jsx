5ceddbbe79ce099d72b70a16eb36f9610bb6df40
Ahir Anto <antuvhai@gmail.com>
2025-08-25 02:16:44 -0700

    implement tanstack query and active cart icon and more functions

--- src/main.jsx
+++ src/main.jsx
@@ -8,13 +8,20 @@ import router from './routes/Router.jsx';
 import { HelmetProvider } from '@dr.pogodin/react-helmet';
 import AuthProvider from './providers/AuthProvider.jsx';
 import { Toaster } from 'react-hot-toast';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+const queryClient = new QueryClient()
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HelmetProvider>
       <AuthProvider>
+        <QueryClientProvider client={queryClient}>
+      <HelmetProvider>
     <RouterProvider router={router} />
     <Toaster position='top-center' reverseOrder={false}></Toaster>
-      </AuthProvider>
     </HelmetProvider>
+    </QueryClientProvider>
+      </AuthProvider>
   </StrictMode>,
 )
