bb91ad5c68af15af5eb0e981da995bf29f63537e
Ahir Anto <antuvhai@gmail.com>
2025-09-24 08:12:16 -0700

    added stripe payment system

--- src/DashboardComponents/Payment.jsx
+++ src/DashboardComponents/Payment.jsx
@@ -1,10 +1,18 @@
+import { loadStripe } from "@stripe/stripe-js";
 import TitleBox from "../components/TitleBox";
+import { Elements } from "@stripe/react-stripe-js";
+import CheckOutForm from "./CheckOutForm";
 
-
+const stripePromise = loadStripe('');
 const Payment = () => {
     return (
         <div>
             <TitleBox title={'--Payment--'} heading={'Pay Your Bill'}></TitleBox>
+            <div>
+                <Elements stripe={stripePromise}>
+                    <CheckOutForm></CheckOutForm>
+                </Elements>
+            </div>
         </div>
     );
 };
